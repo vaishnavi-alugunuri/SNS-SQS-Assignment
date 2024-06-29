899e54ec919df70c403c92af28b18e6611d4cfbf
vaishnavi-alugunuri <vaishnavi.alugunuri@practo.com>
2024-06-29 17:02:12 +0530

    Update producer.py

--- SNS-SQS/sns-sqs-app/producer.py
+++ SNS-SQS/sns-sqs-app/producer.py
@@ -10,17 +10,33 @@ endpoint_url = 'http://localhost:4566'
 
 sns = boto3.client('sns', endpoint_url=endpoint_url)
 
+def create_or_get_topic(topic_name):
+    try:
+        response = sns.create_topic(Name=topic_name)
+        topic_arn = response['TopicArn']
+        print(f"Created topic: {topic_name} with ARN: {topic_arn}")
+    except sns.exceptions.TopicLimitExceededException:
+        print(f"Topic {topic_name} already exists")
+        response = sns.list_topics()
+        topics = response['Topics']
+        topic_arn = next((t['TopicArn'] for t in topics if t['TopicArn'].endswith(topic_name)), None)
+        if topic_arn:
+            print(f"Using existing topic: {topic_name} with ARN: {topic_arn}")
+        else:
+            raise Exception(f"Topic {topic_name} not found even though it should exist!")
+    return topic_arn
+
 def publish_message(event_type, message):
     if event_type == 'broadcast':
-        topic_arn = 'arn:aws:sns:us-east-1:000000000000:broadcast'
+        topic_arn = create_or_get_topic('broadcast')
     elif event_type == 'communication':
-        topic_arn = 'arn:aws:sns:us-east-1:000000000000:communication'
+        topic_arn = create_or_get_topic('communication')
     elif event_type == 'entity':
-        topic_arn = 'arn:aws:sns:us-east-1:000000000000:entity'
+        topic_arn = create_or_get_topic('entity')
     else:
         print("Invalid event type")
         return
-    
+
     sns.publish(TopicArn=topic_arn, Message=message)
     print(f"Message published to {event_type} topic")
 
