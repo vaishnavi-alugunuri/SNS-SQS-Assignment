669eb217f64f8963d848291dada08472f8cf066a
vaishnavi-alugunuri <vaishnavi.alugunuri@practo.com>
2024-06-29 17:03:11 +0530

    Update consumer.py

--- SNS-SQS/sns-sqs-app/consumer.py
+++ SNS-SQS/sns-sqs-app/consumer.py
@@ -11,8 +11,20 @@ endpoint_url = 'http://localhost:4566'
 
 sqs = boto3.client('sqs', endpoint_url=endpoint_url)
 
+def create_or_get_queue(queue_name):
+    try:
+        response = sqs.create_queue(QueueName=queue_name)
+        queue_url = response['QueueUrl']
+        print(f"Created queue: {queue_name} with URL: {queue_url}")
+    except sqs.exceptions.QueueNameExists:
+        print(f"Queue {queue_name} already exists")
+        response = sqs.get_queue_url(QueueName=queue_name)
+        queue_url = response['QueueUrl']
+        print(f"Using existing queue: {queue_name} with URL: {queue_url}")
+    return queue_url
+
 def consume_messages(queue_name):
-    queue_url = sqs.get_queue_url(QueueName=queue_name)['QueueUrl']
+    queue_url = create_or_get_queue(queue_name)
 
     while True:
         response = sqs.receive_message(
@@ -34,5 +46,8 @@ if __name__ == "__main__":
         print("Usage: python consumer.py <queue_name>")
         sys.exit(1)
     
+    queue_name = sys.argv[1]  # 'email', 'sms', or 'entity'
+    consume_messages(queue_name)
+
     queue_name = sys.argv[1]  # 'email', 'sms', or 'entity'
     consume_messages(queue_name)
